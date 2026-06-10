a974530a0ad5de91df1a128fb22d1c6bc3fe347c
PrathapDev03 <prathap03.p@gmail.com>
2026-06-10 21:23:53 +0530

    Finall commit

--- backend/db.py
+++ backend/db.py
@@ -6,10 +6,7 @@ from urllib.parse import quote_plus
 # Read values from ECS Environment Variables
 DB_USER = os.getenv("DB_USER", "admin")
 DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "Admin121"))
-DB_HOST = os.getenv(
-    "DB_HOST",
-    "employee-db-task3.cbuoa8kc81xo.ap-southeast-1.rds.amazonaws.com"
-)
+DB_HOST = os.getenv("DB_HOST", "employee-db.cbuoa8kc81xo.ap-southeast-1.rds.amazonaws.com")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "employee_management")
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,25 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://employee-backend-alb-1864446283.ap-southeast-1.elb.amazonaws.com"
+  baseURL: "http://ac282588713a24d6e934ff5b5bffef02-1266472394.ap-southeast-1.elb.amazonaws.com"
 });
+
+API.interceptors.request.use(
+  (config) => {
+
+    const token =
+      localStorage.getItem("token");
+
+    if (token) {
+
+      config.headers.Authorization =
+        `Bearer ${token}`;
+
+    }
+
+    return config;
+
+  }
+);
 
 export default API;
